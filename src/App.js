2cdee0157ba63794c3a9392fae1f24db9d74802a
obamus <61189062+obamus@users.noreply.github.com>
2020-02-27 14:42:04 +0000

    Moved LoginForm to separate file

--- src/App.js
+++ src/App.js
@@ -1,60 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import LoginForm from './LoginForm';
 export default wrapper;
 
 function wrapper(){
 	return(<div align="center">
-		<Title />
 		<LoginForm />
 		</div>
 	);
 }
-
-function Title(props) {
-	return <h1>reView</h1>;
-}
-
-class LoginForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {username: '', password: ''};
-
-		this.handleUsernameChange = this.handleUsernameChange.bind(this);
-		this.handlePasswordChange = this.handlePasswordChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleUsernameChange(event) {
-		this.setState({
-			username: event.target.value});
-	}
-	handlePasswordChange(event) {
-		this.setState({password: event.target.value});
-	}
-
-	handleSubmit(event) {
-		alert('Login submitted: ' + this.state.username + ' ' + this.state.password);
-		event.preventDefault();
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-			<div>
-			<div> Username: </div>
-			<input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
-			</div>
-			<div>
-			<div> Password: </div>
-			<input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
-			</div>
-			<button> Reset </button> {" "}
-			<input type="submit" value="Submit"/>
-			</form>
-		);
-	}
-}
-
 
